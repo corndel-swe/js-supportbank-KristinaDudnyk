a4831785b67ed5ea2471cad53557e2756c9d19ec
KristinaDudnyk <kristina.dudnyk20@gmail.com>
2024-12-16 11:12:30 +0000

    workshop day1

--- cli/bill.js
+++ cli/bill.js
@@ -4,11 +4,11 @@ import Bill from '../models/Bill.js'
 const billController = new Command('bill')
 
 billController
-  .command('split <total> <numOfPeople>')
+  .command('split <total> <numOfPeople> [tip]')
   .description('Log transaction data to the console')
-  .action((total, numOfPeople) => {
+  .action((total, numOfPeople, tip) => {
     const splitedBill = new Bill()
-    console.log(splitedBill.split(total, numOfPeople))
+    console.log(splitedBill.split(total, numOfPeople, tip || 0))
   })
 
 export default billController
--- models/Bill.js
+++ models/Bill.js
@@ -1,11 +1,19 @@
 class Bill {
-  constructor(total, numOfPeople) {
+  constructor(total, numOfPeople, tip) {
     this.total = total
     this.numOfPeople = numOfPeople
+    this.tip = tip
   }
 
-  split(total, numOfPeople) {
-    return Math.round(total / numOfPeople)
+  split(total, numOfPeople, tip) {
+    if (numOfPeople === 0) {
+      throw new Error('provide a number of people')
+    }
+    if (!tip) {
+      tip = 0
+    }
+    const newTotal = total - tip
+    return Math.round(newTotal / numOfPeople)
   }
 }
 
